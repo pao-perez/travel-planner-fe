212ed463ccc9a706ae69a6290afd5a90328d4ac7
Pao Perez <pperez@hyperglance.com>
2024-09-03 21:40:07 -0600

    style: set min-width to 50% of viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <main className="flex justify-center min-h-screen bg-gray-100 p-6">
-      <article className="max-w-md bg-white shadow-md rounded-md p-6">
+      <article className="max-w-md min-w-[50vw] bg-white shadow-md rounded-md p-6">
         <section className="flex flex-col gap-4">
           <header>
             <h1 className="text-2xl font-bold">Trip Planner</h1>
